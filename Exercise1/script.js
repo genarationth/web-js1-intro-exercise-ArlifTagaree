710789c3b31bf368f7e3f7d0c3d3b2b91dc7e57d
ArlifTagaree <liffism9@gmail.com>
2023-03-10 11:54:19 +0700

    edit script add promt box

---
+++ Exercise1/script.js
@@ -0,0 +1,7 @@
+function ChangeName() {
+    let text;
+    let person = prompt("What is your name?", "Arlif");
+    text = "Hello " + person + "! How are you today?";
+    document.getElementById("inputName").innerHTML = text;
+}
+
